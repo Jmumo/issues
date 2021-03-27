b07ea5bcb63f1e6be1b442e694b7273a412e9b56
Orinda Harrison <ajuongharry35@gmail.com>
2021-03-27 19:54:22 +0300

    Dispatching repository list directly

--- src/actions/fetchRepos.js
+++ src/actions/fetchRepos.js
@@ -1,31 +1,20 @@
-
 import axios from 'axios';
 import github_data from '../constants/const.js';
-import body from '../constants/queries'
+import body from '../constants/queries';
 
-const headers = {
-  "Content-Type":"application/json",
- "Authorization":"bearer"+github_data.key,
-}
 
-export function fetchrepos(){
-    return function (dispatch){
-      axios.post(
-      github_data.baseurl,{
-        query:body
-      }
-      ,{
-             headers:headers
-      }
-       
-    
-    ).then(res =>{
-
-        dispatch({type:"FETCH_REPOS", payload:res})
-
-    }).catch( err =>{
-        console.log(err)
-    })
-
+export function fetchrepos() {
+    return function (dispatch) {
+        axios.post(github_data.baseurl, {query: body}, {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${github_data.key}`,
+                }
+            }
+        ).then(res => {
+            dispatch({type: "FETCH_REPOS", payload: res.data.data.user.repositories.nodes});
+        }).catch(err => {
+            console.error(err);
+        });
     }
 }
