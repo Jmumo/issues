efc28d5154f9a2b5fef4db075d6ad84f93f29e04
Orinda Harrison <ajuongharry35@gmail.com>
2021-03-27 19:52:34 +0300

    Use more descriptive funtion name

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,12 +1,9 @@
-import { combineReducers } from 'redux'
-import  repoList  from './reporeducer'
+import {combineReducers} from 'redux';
+import repositoriesReducer from './reporeducer';
 
-//  export const issuereducer = (state,action)=>{
-//   return state
-// }
 
 const reducers = combineReducers({
-    repoList:repoList
+    repositories: repositoriesReducer
 })
 
 export default reducers;
