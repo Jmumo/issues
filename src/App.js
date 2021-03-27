6014e036642e8cd476293402b172d20b96c5b2a2
Orinda Harrison <ajuongharry35@gmail.com>
2021-03-27 20:40:19 +0300

    Refactor for readability

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-
 import './App.css';
-import Home from './components/Home'
-import { Provider } from 'react-redux'
- import Store from './store'
+import Home from './components/Home';
+import {Provider} from 'react-redux';
+import store from './store';
 
-function App() {
-
-  return (
-    <Provider store = { Store }>
-  <div className="container text-center">
-    <Home/>
-  </div>
-  </Provider>
-
-
-  );
-}
-
-export default App;
+export default function App() {
+    return (
+        <Provider store={store}>
+            <div className="container text-center">
+                <Home/>
+            </div>
+        </Provider>
+    );
+};
