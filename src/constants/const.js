a2e0323019bee451b623aa3e5974040d50eb1c85
Orinda Harrison <ajuongharry35@gmail.com>
2021-03-27 20:39:11 +0300

    Load api tokens from environment variables for security

--- src/constants/const.js
+++ src/constants/const.js
@@ -1,7 +1,6 @@
-
 const github_data = {
-  "key":" ca797bd4b0238d9228e76cbab1ac89664f5c6a60",
-  "username":"Jmumo",
-  "baseurl":"https://api.github.com/graphql"
+    "key": process.env.GITHUB_API_KEY || "ca797bd4b0238d9228e76cbab1ac89664f5c6a60",
+    "username": process.env.GITHUB_USERNAME || "Jmumo",
+    "baseurl": "https://api.github.com/graphql"
 }
 module.exports = github_data
