4324c38ca308390530362ba754d0fcae83275ab6
Orinda Harrison <ajuongharry35@gmail.com>
2021-03-27 20:38:03 +0300

    Move query body to where it's used

--- src/actions/fetchRepos.js
+++ src/actions/fetchRepos.js
@@ -1,11 +1,21 @@
 import axios from 'axios';
 import github_data from '../constants/const.js';
-import body from '../constants/queries';
 
 
 export function fetchrepos() {
     return function (dispatch) {
-        axios.post(github_data.baseurl, {query: body}, {
+        const query = `query { 
+ user(login:"${github_data.username}"){
+  repositories(last:10,orderBy: {field:CREATED_AT,direction:DESC}){
+    nodes {
+      createdAt
+      pushedAt
+      name
+    }
+  }
+}
+}`;
+        axios.post(github_data.baseurl, {query: query}, {
                 headers: {
                     "Content-Type": "application/json",
                     "Authorization": `Bearer ${github_data.key}`,
